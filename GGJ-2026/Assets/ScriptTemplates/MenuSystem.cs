5115ef057d186f992924a090a42dca2debf45a48
MMAGES2006 <22170@portalcedi.edu.mx>
2026-01-31 15:24:08 -0600

    IDK

---
+++ GGJ-2026/Assets/ScriptTemplates/MenuSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Virus
+{
+    public class MenuSystem : MonoBehaviour
+    {
+        public void Jugar()
+        {
+            SceneManager
+        }
+        
+    }
+}
