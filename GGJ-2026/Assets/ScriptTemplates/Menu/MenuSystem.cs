8a4bba7f2c3658e74e98e3b56cbf4da641a85954
SomarArnulfo048 <ar168fo@gmail.com>
2026-01-31 16:22:12 -0600

    Sistema del Menu v1

--- GGJ-2026/Assets/ScriptTemplates/Menu/MenuSystem.cs
+++ GGJ-2026/Assets/ScriptTemplates/Menu/MenuSystem.cs
@@ -1,13 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
+    [Header("Navegación")]
+    public GameObject panelMenu;     
+    public GameObject panelOpciones; 
+    public GameObject panelCreditos; 
+
+    [Header("Configuración de Audio")]
+    public Toggle toggleMusica;
+    public Toggle toggleSonidos;
+
+    // Menu Principal
+
     public void Jugar()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene"); // Cambiar nombre a la escena de inicio del juego
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+
+    // Paneles de Navegación
+
+    public void AbrirOpciones()
+    {
+        panelMenu.SetActive(false);
+        panelOpciones.SetActive(true);
+    }
+
+    public void AbrirCreditos() 
+    {
+        panelMenu.SetActive(false);
+        panelCreditos.SetActive(true);
+    }
+
+    // Cerrar Paneles
+
+    public void CerrarOpciones()
+    {
+        panelOpciones.SetActive(false);
+        panelMenu.SetActive(true);
+    }
+
+    public void CerrarCreditos() 
+    {
+        panelCreditos.SetActive(false);
+        panelMenu.SetActive(true);
+    }
+
+    // Menu de audio
+
+    public void CambiarMusica(bool encendido)
+    {
+        if (encendido) Debug.Log("Música ON");
+        else Debug.Log("Música OFF");
+    }
+
+    public void CambiarSonidos(bool encendido)
+    {
+        if (encendido) Debug.Log("Sonidos ON");
+        else Debug.Log("Sonidos OFF");
     }
 }
-
