c77b2255d89fae0a6e5913c35e1109f423fccd52
CVG42 <zap653@amerike.edu.mx>
2026-01-31 09:13:51 -0600

    Health UI change

---
+++ GGJ-2026/Assets/_Project/_Scripts/General/Enums.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GGJ
+{
+    public enum PlatformPowerState
+    {
+        Power1,
+        Power2
+    }
+}
--- GGJ-2026/Assets/_Project/_Scripts/UI/HUD/Player/Health/HearthSpriteController.cs
+++ GGJ-2026/Assets/_Project/_Scripts/UI/HUD/Player/Health/HearthSpriteController.cs
@@ -3,10 +3,8 @@ using UnityEngine.UI;
 
 namespace GGJ
 {
-    public class HearthUIController : MonoBehaviour
+    public class HearthSpriteController : MonoBehaviour
     {
-        private const float DAMAGE_PER_HEART_UNIT = 0.5f;
-
         [Header("Hearts")]
         [SerializeField] private Image[] _hearts;
 
@@ -14,59 +12,18 @@ namespace GGJ
         [SerializeField] private Sprite _power1Sprite;
         [SerializeField] private Sprite _power2Sprite;
 
-        private int _currentHeartIndex;
-        private float _currentHeartFill = 1f;
-
         private PlatformPowerState _currentPowerState = PlatformPowerState.Power1;
-
-        private void Awake()
-        {
-            _currentHeartIndex = _hearts.Length - 1;
-            _currentHeartFill = _hearts[_currentHeartIndex].fillAmount;
-        }
 
         private void Start()
         {
-            PlayerHealthEvents.OnDamageTaken += OnDamageTaken;
-
             InputManager.Source.OnPlatformPower1 += OnPlatformPower1;
             InputManager.Source.OnPlatformPower2 += OnPlatformPower2;
         }
 
         private void OnDestroy()
         {
-            PlayerHealthEvents.OnDamageTaken -= OnDamageTaken;
-
             InputManager.Source.OnPlatformPower1 -= OnPlatformPower1;
             InputManager.Source.OnPlatformPower2 -= OnPlatformPower2;
-        }
-
-        private void OnDamageTaken(int damageAmount)
-        {
-            float visualDamage = damageAmount * DAMAGE_PER_HEART_UNIT;
-            ApplyVisualDamage(visualDamage);
-        }
-
-        private void ApplyVisualDamage(float damage)
-        {
-            while (damage > 0 && _currentHeartIndex >= 0)
-            {
-                float amountToRemove = Mathf.Min(damage, _currentHeartFill);
-
-                _currentHeartFill -= amountToRemove;
-                _hearts[_currentHeartIndex].fillAmount = _currentHeartFill;
-
-                damage -= amountToRemove;
-
-                if (_currentHeartFill <= 0f)
-                {
-                    _currentHeartIndex--;
-                    if (_currentHeartIndex >= 0)
-                    {
-                        _currentHeartFill = _hearts[_currentHeartIndex].fillAmount;
-                    }
-                }
-            }
         }
 
         private void OnPlatformPower1()
@@ -93,11 +50,6 @@ namespace GGJ
             {
                 heart.sprite = spriteToUse;
             }
-        }
-
-        public bool IsDead()
-        {
-            return _currentHeartIndex < 0;
         }
     }
 }
